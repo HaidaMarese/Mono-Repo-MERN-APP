872fcac004fe42a9eef3c4869c622e36c8165335
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-07-28 15:31:36 -0400

    backend url updated

--- frontend/src/clients/backendClient.js
+++ frontend/src/clients/backendClient.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 
 export const backendClient = axios.create({
-    baseURL: `http://localhost${import.meta.env.PORT}/api`,
+    baseURL: `http://localhost:10000/api`,
 });
